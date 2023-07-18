ac1f19b24e506d98c8b628009f689748047db879
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-07-17 20:44:46 -0400

    ejercicio 14 diseño completado

---
+++ app/src/main/java/com/example/cl/ejercicio14kotlinandroidmodulo5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cl.ejercicio14kotlinandroidmodulo5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
